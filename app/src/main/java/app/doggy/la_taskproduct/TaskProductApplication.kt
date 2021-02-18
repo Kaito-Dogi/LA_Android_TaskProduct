4de4b5152afeb3700853e257e9e674804f9b675d
Kaito-Dogi <kendobu0405@gmail.com>
2021-02-18 16:28:32 +0900

    Applicationクラスを作成

---
+++ app/src/main/java/app/doggy/la_taskproduct/TaskProductApplication.kt
@@ -0,0 +1,18 @@
+package app.doggy.la_taskproduct
+
+import android.app.Application
+import android.util.Log
+import io.realm.Realm
+import io.realm.RealmConfiguration
+
+class TaskProductApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        Realm.init(this)
+        val realmConfig = RealmConfiguration.Builder()
+            .deleteRealmIfMigrationNeeded()
+            .build()
+        Realm.setDefaultConfiguration(realmConfig)
+        Log.d("TaskProductApplication", "TaskProductApplication has been created.")
+    }
+}
