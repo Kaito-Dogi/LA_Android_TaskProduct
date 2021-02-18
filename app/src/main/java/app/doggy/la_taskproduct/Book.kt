f1ab02501708db0969c549ef20751de622decefd
Kaito-Dogi <kendobu0405@gmail.com>
2021-02-18 16:47:41 +0900

    Bookクラス作成

---
+++ app/src/main/java/app/doggy/la_taskproduct/Book.kt
@@ -0,0 +1,15 @@
+package app.doggy.la_taskproduct
+
+import io.realm.RealmObject
+import io.realm.annotations.PrimaryKey
+import java.util.*
+
+open class Book(
+    @PrimaryKey open var id: String = UUID.randomUUID().toString(),
+    open var bookImageId: Int = 0,
+    open var title: String = "",
+    open var author: String = "",
+    open var price: Int = 0,
+    open var content: String = "",
+    open var createdAt: Date = Date(System.currentTimeMillis())
+) : RealmObject()
