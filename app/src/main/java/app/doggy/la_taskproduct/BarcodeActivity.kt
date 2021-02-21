b38f8e8c6958e5c034343ba4fd01f73bcc5c7759
Kaito-Dogi <kendobu0405@gmail.com>
2021-02-22 01:14:14 +0900

    zxing実装完了

--- app/src/main/java/app/doggy/la_taskproduct/BarcodeActivity.kt
+++ app/src/main/java/app/doggy/la_taskproduct/BarcodeActivity.kt
@@ -3,15 +3,35 @@ package app.doggy.la_taskproduct
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 import android.widget.Toast
+import com.google.zxing.integration.android.IntentIntegrator
 import kotlinx.android.synthetic.main.activity_barcode.*
 
 class BarcodeActivity : AppCompatActivity() {
+
+    private var qrScanIntegrator: IntentIntegrator? = null
+
+    lateinit var isbnEditText: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_barcode)
 
         supportActionBar?.hide()
+
+        isbnEditText = findViewById(R.id.isbnEditText)
+
+        qrScanIntegrator = IntentIntegrator(this)
+
+        // 画面の回転をさせない (今回は縦画面に固定)
+        qrScanIntegrator?.setOrientationLocked(false)
+
+        // QR 読み取り後にビープ音がなるのを止める
+        qrScanIntegrator?.setBeepEnabled(false)
+
+        // スキャン開始 (QR アクティビティ生成)
+        qrScanIntegrator?.initiateScan()
 
         topAppBarBarcode.setOnMenuItemClickListener { menuItem ->
             when (menuItem.itemId) {
@@ -39,5 +59,20 @@ class BarcodeActivity : AppCompatActivity() {
 
         }
 
+    }
+
+    // 読み取り後に呼ばれるメソッド
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        // 結果の取得
+        val result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
+
+        if (result != null) {
+            // result.contents で取得した値を参照できる
+            isbnEditText.setText(result.contents)
+        }
+
+        else {
+            super.onActivityResult(requestCode, resultCode, data)
+        }
     }
 }
